4cba4da9dbda6ecd57a94500f5f57e2c90955b9e
snoopyods <snoopyods@gmail.com>
2023-09-01 11:54:22 -0700

    Added basic structure and backend

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import Home from "./scenes/home/Home";
+
+const ScrollTotop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname])
+
+  return null;
+}
+
+function App() {
+  return <div className="app">
+      <BrowserRouter>
+        <ScrollTotop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+      </BrowserRouter>
+    </div>;
+}
+
+export default App;
